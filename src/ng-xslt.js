3ed0d6f89aab5b299da056cc8b9c78f85d8671fb
Nikita Khomyakov <dr.evil@krtn.eu>
2015-05-27 21:22:21 +0100

    refactor: removed legacy code, improved IE support and error handling Dropped legacy support for browsers that are not supported by AngularJS IE support is done right way Parser error description is included from now on

--- src/ng-xslt.js
+++ src/ng-xslt.js
@@ -1,55 +1,93 @@
-angular.module('ngXslt', [])
-    .filter('xslt', [
-        '$window', '$document',
-        function ($window, $document) {
-            /**
-             * Returns whether the browser supports XSLT.
-             *
-             * @return the browser supports XSLT
-             * @type boolean
-             */
-            function browserSupportsXSLT () {
-                if ($document.recalc) // IE 5+
-                    return true;
-                else if (angular.isDefined($window.XMLHttpRequest) && angular.isDefined($window.XSLTProcessor)) { // Mozilla 0.9.4+, Opera 9+
-                    if (typeof (new XSLTProcessor()).transformDocument === 'function')
-                        return angular.isDefined($window.XMLSerializer);
+(function () {
+    /**
+     * Returns whether the browser supports XSLT.
+     *
+     * @return the browser supports XSLT
+     * @type boolean
+     */
+    function browserSupportsXSLT (window) {
+        return ('ActiveXObject' in window || // IE
+                (angular.isDefined(XSLTProcessor) && angular.isDefined(XMLSerializer)));
+    }
 
-                    return true;
-                }
-                return false;
-            }
+    /**
+     * Searches for parsererror in given Document and returns first line of content,
+     * which usually contains brief description of error cause
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/DOMParser
+     *
+     * @param {Document} document
+     * @returns {String} error
+     */
+    function getParserError (document) {
+        var err;
+        if (err = document.getElementsByTagName('parsererror')[0])
+            return err.textContent ? err.textContent.split('\n', 1)[0] : 'unknown parser error';
 
-            return function (xml, xslt) {
-                if(!xml || !xslt)
-                    return ('No' + (!xml ? ' XML' : '') + (!xml && !xslt ? ' &' : '') + (!xslt ? ' XSLT' : '')).trim();
+        return null;
+    }
 
-                if (!browserSupportsXSLT())
-                    return 'Not supported by your browser';
+    /**
+     * Transforms XML using XSLT
+     *
+     * @param {Window} window
+     * @param {Window} document
+     * @param {String} xml      Source XML that is going to be transformed
+     * @param {String} xslt     XSL template that is applied for the XML transformation
+     *
+     * @returns {String}
+     */
+    function transformXml (window, document, xml, xslt) {
+        if (!xml || !xslt)
+            return 'No' + (!xml ? ' XML' : '') + (!xml && !xslt ? ' &' : '') + (!xslt ? ' XSLT' : '');
 
-                xml = (new DOMParser()).parseFromString(xml, 'text/xml');
-                if(xml.getElementsByTagName('parsererror')[0])
-                    return 'Invalid XML';
+        if (!browserSupportsXSLT(window))
+            return 'XSL transformation is not supported by your browser';
 
-                xslt = (new DOMParser()).parseFromString(xslt, 'text/xml');
+        var processor, output, xmlDoc, xsltDoc;
+        if ('ActiveXObject' in window) { // IE
+            xmlDoc = new ActiveXObject('Msxml2.DOMDocument');
+            xmlDoc.loadXML(xml);
+            if (xmlDoc.parseError.errorCode !== 0)
+                return ('Invalid XML (' + xmlDoc.parseError.errorCode + '): ' + xmlDoc.parseError.reason).trim();
 
-                var processor = new XSLTProcessor();
+            xsltDoc = new ActiveXObject('Msxml2.FreeThreadedDOMDocument');
+            xsltDoc.loadXML(xslt);
+            if (xsltDoc.parseError.errorCode !== 0)
+                return ('Invalid XSLT (' + xsltDoc.parseError.errorCode + '): ' + xsltDoc.parseError.reason).trim();
 
-                var output;
-                if (typeof processor.transformDocument === 'function') { // obsolete Mozilla interface
-                    output = document.implementation.createDocument("", "", null);
-                    processor.transformDocument(xml, xslt, output, null);
-                    return (new XMLSerializer()).serializeToString(output);
-                } else {
-                    processor.importStylesheet(xslt);
-                    output = processor.transformToFragment(xml, document);
+            var template = new ActiveXObject('Msxml2.XSLTemplate');
+            template.stylesheet = xsltDoc;
 
-                    if(!output)
-                        return 'Invalid XSLT';
+            processor = template.createProcessor();
+            processor.input = xmlDoc;
+            processor.transform();
 
-                    var div = document.createElement('div');
-                    div.appendChild(output);
-                    return div.innerHTML;
-                }
-            }
-        }]);
+            output = processor.output;
+        } else {
+            var err;
+
+            xmlDoc = (new DOMParser()).parseFromString(xml, 'text/xml');
+            if (err = getParserError(xmlDoc))
+                return 'Invalid XML: ' + err;
+
+            xsltDoc = (new DOMParser()).parseFromString(xslt, 'text/xml');
+            if (err = getParserError(xsltDoc))
+                return 'Invalid XSLT: ' + err;
+
+            processor = new XSLTProcessor();
+            processor.importStylesheet(xsltDoc);
+
+            output = (new XMLSerializer()).serializeToString(processor.transformToFragment(xmlDoc, document));
+        }
+
+        return output || '(empty)';
+    }
+
+    angular.module('ngXslt', [])
+        .filter('xslt', [
+            '$window', '$document',
+            function ($window, $document) {
+                return transformXml.bind(this, $window, $document[0]);
+            }]);
+})();
